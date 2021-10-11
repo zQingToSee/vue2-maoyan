07bff0b9560aae9f5560e30379c87a5306b860cb
zQing <18356951603@163.com>
2021-10-11 17:44:25 +0800

    v1.0.9

--- src/views/City.vue
+++ src/views/City.vue
@@ -1,21 +1,132 @@
 <template>
-  <div>城市选择页面</div>
+  <div>
+    <section class="hot">
+      <div class="title">热门城市</div>
+      <ul>
+        <li v-for="city in hotCities.cities" :key="city.cityId" class="li">
+          {{ city.name }}
+        </li>
+      </ul>
+    </section>
+    <section v-for="item in otherCities" :key="item.prefix">
+      <div class="title">{{ item.prefix | toUpperCase }}</div>
+      <ul>
+        <li v-for="city in item.cities" :key="city.cityId" class="li">
+          {{ city.name }}
+        </li>
+      </ul>
+    </section>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      cityList: [],
+    };
   },
-
-  components: {},
-
-  computed: {},
-
-  mounted() {},
-
-  methods: {},
+  created() {
+    this.getCityList(); // 请求城市数据
+  },
+  computed: {
+    hotCities() {
+      return this.cityList[0];
+    },
+    otherCities() {
+      return this.cityList.slice(1);
+    },
+  },
+  filters: {
+    toUpperCase(val) {
+      return val.toUpperCase();
+    },
+  },
+  methods: {
+    getCityList() {
+      fetch("http://www.pudge.wang:3080/api/area/list")
+        .then((res) => res.json())
+        .then((res) => {
+          console.log(res);
+          this.cityList = res.result;
+        })
+        .catch((err) => console.log(err));
+    },
+  },
 };
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+// section:first-child {
+//   ul {
+//     display: flex;
+//     flex-wrap: wrap; // 换行
+
+//     .li {
+//       margin-left: 15px;
+//       background: #fff;
+//       width: 29%;
+//       height: 33px;
+//       line-height: 33px;
+//       margin-top: 5px;
+//       margin-bottom: 10px;
+//       margin-left: 4%;
+//       padding: 0 4px;
+//       border: 1px solid #e6e6e6;
+//       border-radius: 3px;
+//       text-align: center;
+//       font-size: 14px;
+//       color: #333;
+//     }
+//     .li:nth-child(1),
+//     .li:nth-child(2),
+//     .li:nth-child(3) {
+//       margin-top: 10px;
+//     }
+//   }
+// }
+
+.hot {
+  ul {
+    display: flex;
+    flex-wrap: wrap; // 换行
+
+    .li {
+      margin-left: 15px;
+      background: #fff;
+      width: 29%;
+      height: 33px;
+      line-height: 33px;
+      margin-top: 5px;
+      margin-bottom: 10px;
+      margin-left: 4%;
+      padding: 0 4px;
+      border: 1px solid #e6e6e6;
+      border-radius: 3px;
+      text-align: center;
+      font-size: 14px;
+      color: #333;
+    }
+    .li:nth-child(1),
+    .li:nth-child(2),
+    .li:nth-child(3) {
+      margin-top: 10px;
+    }
+  }
+}
+.title {
+  padding-left: 25px;
+  line-height: 30px;
+  background-color: #ebebeb;
+  font-size: 14px;
+  color: #333;
+}
+
+section:not(:first-child) .li {
+  height: 44px;
+  line-height: 44px;
+  margin-left: 15px;
+  border-bottom: 1px solid #c8c7cc;
+  font-size: 14px;
+}
+</style>
--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>{{ detailInfo.nm }}</div>
+  <!-- 这里的if用于判断数据是否已请求成功，成功后再渲染页面，避免undefined情况 -->
+  <div v-if="detailInfo.nm">
+    {{ detailInfo.nm }}
+    <button @click="guess">猜你喜欢</button>
+    <img :src="detailInfo.img | formaturl" alt="" />
+  </div>
 </template>
 
 <script>
@@ -15,27 +20,57 @@ export default {
   created() {
     this.id = this.$route.params.id;
   },
+  methods: {
+    getData(id) {
+      fetch("http://www.pudge.wang:3080/api/movies/detail", {
+        method: "POST",
+        body: JSON.stringify({
+          id: id,
+        }),
+        headers: {
+          "content-type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((res) => {
+          // console.log(res);
+          this.detailInfo = res.result;
+        });
+    },
+    guess() {
+      this.$router.push("/detail/1240838");
+    },
+  },
+  filters: {
+    formaturl(val) {
+      return val.split("/w.h").join("/100.200");
+    },
+  },
   beforeRouteEnter(to, from, next) {
     NProgress.start();
     fetch("http://www.pudge.wang:3080/api/movies/detail", {
       method: "POST",
       body: JSON.stringify({
         id: to.params.id,
-      }), // data can be `string` or {object}!
-      headers: new Headers({
-        "Content-Type": "application/json",
       }),
+      headers: {
+        "content-type": "application/json",
+      },
     })
-      .then((res) => {
+      .then((response) => {
         NProgress.done();
-        return res.json();
+        return response.json();
       })
-      .catch((error) => console.error("Error:", error))
       .then((res) => {
         next((vm) => {
           vm.detailInfo = res.result;
         });
       });
+  },
+  //* 猜你喜欢要用到beforeRouteUpdate方法
+  beforeRouteUpdate(to, from, next) {
+    this.getData(to.params.id);
+    next();
   },
 };
 </script>
